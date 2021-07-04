f0ae859cdd079fae35ed84fdf43699677e1ed5a8
Tomasz Klara <tomaszklara@gmail.com>
2021-07-04 12:38:25 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
+import React, {Component} from 'react';
+import Pend from './img/penderecki-cherry-actions-front.jpg'
+import Orf from './img/orff-carmina-burana-front.jpg'
+// scss
 import '../src/scss/main.scss'
 
 
-function App() {
-  return (
-    <div>
-      <h1 className="text--md">Lp Collection</h1>
-    </div>
-  );
-}
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="text--md">Lp Collection</h1>
+                <main className="container">
 
-export default App;
+                    <div className="album-container">
+                        <div className="album-card">
+                            <div className="t">
+                                <img src={Pend} alt="actions" />
+                            </div>
+                            <div className="album-flex-t">
+                                <h1 className="text--3xl album-title">Actions</h1>
+                                <span className="text--md album-artist divider">Krzysztof Penderecki</span>
+                            </div>
+
+                            <div className="album-flex-b">
+                                <div className="album-info">
+                                    <div className="album-info--details album-info--divider">
+                                        <p>Label</p>
+
+                                        <span className="text--md divider">CBS</span>
+
+                                    </div>
+                                    <div className="album-info--details">
+                                        <p>Year</p>
+                                        <span className="text--md">1974</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="album-card">
+                            <div className="t">
+                                <img src={Orf} alt="actions" />
+                            </div>
+                            <div className="album-flex-t">
+                                <h1 className="text--3xl album-title">Carmina Burana</h1>
+                                <span className="text--md album-artist divider">Carl Orff</span>
+                            </div>
+
+                            <div className="album-flex-b">
+                                <div className="album-info">
+                                    <div className="album-info--details album-info--divider">
+                                        <p>Label</p>
+
+                                        <span className="text--md divider">CBS</span>
+
+                                    </div>
+                                    <div className="album-info--details">
+                                        <p>Year</p>
+                                        <span className="text--md">1974</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        );
+    }
+}
